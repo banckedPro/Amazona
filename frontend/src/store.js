1cdc880becfc76e6bf8e156645a1f0cf207d0b8f
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-08-29 00:50:36 +0530

    Store Modified

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slice/apiSlice';
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+
+  devTools: true,
 });
 
 export default store;
