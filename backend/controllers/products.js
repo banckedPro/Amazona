1477fdbaaedb70a02c2c4c32e6054c040ec1ee77
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-08-28 18:15:38 +0530

    Added Product Controller

---
+++ backend/controllers/products.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/Product.js';
+
+// @desc     Fetch All Products
+// @route    GET /api/products
+// @access   Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc     Fetch All Products
+// @route    GET /api/products
+// @access   Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error(`Product not Found`);
+  }
+
+  res.json(product);
+});
+
+export { getProduct, getProducts };
