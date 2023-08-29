d37d6203772b189f87cbe6dd4021a1de505e89fa
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-08-29 19:21:04 +0530

    User Password Check Added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema(
   {
@@ -27,6 +28,10 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.methods.matchPasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 
