3fd3ae4c4fc665686ec0bf50dab2e194db7db318
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-08-31 23:12:29 +0530

    Added Shipping Screen

---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const ShippingScreen = () => {
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+
+  return <div>ShippingScreen</div>;
+};
+
+export default ShippingScreen;
