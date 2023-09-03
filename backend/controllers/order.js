147767accbb336b82ec0b01b40dd3d46a293dbcb
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-03 11:43:48 +0530

    Controller updateOrderToDelivered Implemented

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -76,7 +76,18 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route    GET /api/orders/:id/deliver
 // @access   Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('update order to delivered');
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    res.status(404);
+    throw new Error('Order Not Found');
+  }
+
+  order.isDelivered = true;
+  order.deliverdAt = Date.now();
+
+  const updatedOrder = await order.save();
+  res.status(200).json(updatedOrder);
 });
 
 // @desc     Get All Orders
