a3c18f108de89affd4c1541e2f013a05f766a181
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-03 11:16:06 +0530

    Get user order api reducer added

--- frontend/src/slice/ordersApiSlice.js
+++ frontend/src/slice/ordersApiSlice.js
@@ -17,8 +17,18 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+
+    getMyOrders: builder.query({
+      query: () => ({
+        url: `${ORDERS_URL}/mine`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailQuery } =
-  ordersApiSlice;
+export const {
+  useCreateOrderMutation,
+  useGetOrderDetailQuery,
+  useGetMyOrdersQuery,
+} = ordersApiSlice;
