f6d2ee0980d772090d45b3fe10d2b68e3e1435dc
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-03 12:10:59 +0530

    Product List Screen

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import { Button, Col, Row, Table } from 'react-bootstrap';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { useGetProductsQuery } from '../../slice/productApiSlice';
+import Loader from '../../components/Loader';
+import Message from '../../components/Message';
+import { Link } from 'react-router-dom';
 
 const ProductListScreen = () => {
-  return <div>ProductListScreen</div>;
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  const deleteHandler = () => {};
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+
+        <Col className="text-end">
+          <Button className="btn-sm m-3">
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={'danger'}>{error}</Message>
+      ) : (
+        <>
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+
+                  <td>
+                    <Link to={`/admin/product/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm mx-2">
+                        <FaEdit />
+                      </Button>
+                    </Link>
+                    <Button
+                      variant="danger"
+                      className="btn-sm mx-2"
+                      onClick={() => {
+                        deleteHandler(product._id);
+                      }}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProductListScreen;
