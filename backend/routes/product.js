cb89467325ec30946de106652d3e85e013628d03
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-04 22:36:01 +0530

    Routed createProduct

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getProducts, getProductById } from '../controllers/products.js';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+} from '../controllers/products.js';
+
+import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-router.get('/', getProducts);
+router.get('/', getProducts).post(protect, admin, createProduct);
 router.get('/:id', getProductById);
 
 export default router;
