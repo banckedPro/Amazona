01938c28f4d6fae650c2227c7ccc2d88298290f9
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-06 17:44:20 +0530

    Added Product Image Upload Mutation

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import {
   useGetProductByIdQuery,
   useUpdateProductMutation,
+  useUploadProductImageMutation,
 } from '../../slice/productApiSlice';
 import Loader from '../../components/Loader';
 import Message from '../../components/Message';
@@ -14,19 +15,20 @@ import { toast } from 'react-toastify';
 const ProductEditScreen = () => {
   const { id: productId } = useParams();
 
-  const [name, setName] = useState();
-  const [price, setPrice] = useState();
-  const [image, setImage] = useState();
-  const [brand, setBrand] = useState();
-  const [category, setCategory] = useState();
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState('');
+  const [brand, setBrand] = useState('');
+  const [category, setCategory] = useState('');
   const [countInStock, setCountInStock] = useState(0);
-  const [description, setDescription] = useState();
+  const [description, setDescription] = useState('');
 
   const { data: product, isLoading, error } = useGetProductByIdQuery(productId);
 
   const [updateProduct, { isLoading: updateProductLoading }] =
     useUpdateProductMutation();
 
+  const [uploadProductImage] = useUploadProductImageMutation();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -40,6 +42,18 @@ const ProductEditScreen = () => {
       setDescription(product.description);
     }
   }, [product]);
+
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
+    } catch (err) {
+      toast.error(err?.data?.message || err.message);
+    }
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -62,6 +76,7 @@ const ProductEditScreen = () => {
       navigate('/admin/productlist');
     }
   };
+
   return (
     <>
       <Link to={'/admin/productlist'} className="btn btn-light my-3">
@@ -103,7 +118,21 @@ const ProductEditScreen = () => {
                 ></Form.Control>
               </Form.Group>
 
-              {/*INPUT IMAGE PLACEHOLDER*/}
+              <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Choose File</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="IMAGE URL"
+                  readOnly
+                  value={image}
+                  className="my-2"
+                />
+                <Form.Control
+                  type="file"
+                  placeholder={image}
+                  onChange={uploadFileHandler}
+                />
+              </Form.Group>
 
               <Form.Group controlId="brand" className="my-3">
                 <Form.Label>Brand</Form.Label>
