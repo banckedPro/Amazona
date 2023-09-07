64ef0daf74a7349944ffe829ace40bcf5344ffa0
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-07 22:52:06 +0530

    Added Product Review Controller

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -84,10 +84,51 @@ const deleteProduct = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Product Deleted Successfully' });
 });
 
+// @desc     Create a Review
+// @route    POST /api/products/:id/reviews
+// @access   Private/Admin
+const createProductReview = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error(`Resource not found`);
+  }
+
+  const { rating, comment } = req.body;
+
+  const alreadyReviewed = product.reviews.find(
+    (review) => review.user.toString() === req.user._id.toString()
+  );
+
+  if (alreadyReviewed) {
+    res.status(400);
+    throw new Error('Product already reviewed');
+  }
+
+  const review = {
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+    user: req.user._id,
+  };
+
+  product.reviews.push(review);
+  product.numReviews = product.reviews.length;
+
+  product.rating =
+    product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+    product.reviews.length;
+
+  await product.save();
+  res.status(201).json({ message: 'Review Added' });
+});
+
 export {
   getProductById,
   getProducts,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
