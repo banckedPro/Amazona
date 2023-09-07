8addc2f12768f9d4276f6f74b9d15706b2287cb0
Swastik Chaudhary <swastikkumar50@gmail.com>
2023-09-07 23:18:45 +0530

    Added Timestamps for Reviews

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,26 +1,31 @@
 import mongoose from 'mongoose';
 
-const reviewSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-  name: {
-    type: String,
-    required: true,
-  },
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
 
-  rating: {
-    type: Number,
-    required: true,
-  },
+    rating: {
+      type: Number,
+      required: true,
+    },
 
-  comment: {
-    type: String,
-    required: true,
+    comment: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const ProductSchema = new mongoose.Schema(
   {
